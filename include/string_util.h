939fd877f19079e3a0019c6f48180dcc368ab4c9
dor283 <sdor2803@gmail.com>
2024-04-05 09:43:55 +0300

    update string util

---
+++ include/string_util.h
@@ -0,0 +1,33 @@
+/*
+ * The String util:
+ * this string util provide the String struct and its helper functions for easy use of strings in c.
+ * 
+ * How the String struct works:
+ * each string is allocated with memory and every time that we want to add stuff to it we just create a new buffer with double the size.
+ * in the start we will allocate a buffer of 8 chars (inculding the \0 char) and if we see that the index that points to the \0 char in the
+ * struct is in the end of the buffer we would allocate the buffer again with more memory.
+*/
+
+#ifndef LABRATORY_C_FINAL_PROJECT_STRING_UTIL
+#define LABRATORY_C_FINAL_PROJECT_STRING_UTIL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct String {
+    unsigned int size;  /* the size of the buffer allocated to storing the string data (including the \0 char) */
+    unsigned int index; /* the index of the string that has the end of the string, the \0 char */
+    char * data; /* a pointer to the data */
+} String;
+
+String string_init(); /* init string with a buffer of nothing */
+String string_init_with_data(char * data); /* init string with the data from "data" and with a buffer's size of the uppder cilling of 2^x */
+void string_info(String str); /* print string information */
+void string_add_char(String * str, char ch); /* add a char to the end of the string, if the buffer is full reallocated the buffer with double the size*/
+void string_add_char_pointer(String * str, char * toAdd); /* add a char * data to the end of the string, if the buffer is full reallocated the buffer with double the size*/
+void string_add_string(String * str, String toAdd); /* add a String to the end of the str */
+char string_get_char(String str, unsigned int index); /* a get function for getting a char by index from the string, if the index exied the length of string (over or equal to the index of \0) returns \0*/
+bool string_equals(String str1, String str2); /* ckeck if two strings are equals */
+
+#endif /* LABRATORY_C_FINAL_PROJECT_STRING_UTIL */
---
+++ utils/string_util.c
@@ -0,0 +1,110 @@
+#include <string.h>
+#include <stdbool.h>
+#include "../include/string_util.h"
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+static unsigned power_2_ceil(unsigned x);
+static unsigned power_2_floor(unsigned x);
+
+String string_init(){
+    String str;
+    str.size = 8; /* just a nice number, 2^3*/
+    str.index = 0; /* we setup the string data to have no data so the first chari n the buffer will be \0*/
+    str.data = (char *)calloc(8, sizeof sizeof(char));
+
+    return str;
+}
+
+String string_init_with_data(char * data){
+    String str;
+    str.size = MAX(power_2_ceil(strlen(data) + 1), 8); /* strlen(data) for the data itself and + 1 for \0*/
+    str.index = strlen(data);
+    str.data = (char *)malloc(str.size /* *sizeof(char)*/);
+
+    int i;
+    for (i = 0; i < str.index; i++){
+        str.data[i] = data[i];
+    }
+    str.data[str.index] = '\0';
+
+    return str;
+}
+
+void string_add_char(String *str, char ch){
+    if (ch == '\0') return; /* i think i don't need to explain why we to check that */
+    
+    if (str->index + 1 == str->size) /* check if we got to the end (check if we currently don't have enough memory to add a char for the string)*/
+    {
+        str->size *= 2;
+        str->data = realloc(str->data, str->size * 2); /* double the buffer size*/
+        
+    }
+
+    str->data[str->index] = ch;
+    str->index++;
+    str->data[str->index] = '\0';
+}
+
+void string_info(String str){
+    printf("String size: %d \n", str.size);
+    printf("String \\0 index: %d \n", str.index);
+    printf("String size: %s \n", str.data);
+}
+
+void string_add_char_pointer(String * str, char * toAdd){
+    if (toAdd == NULL) return; /* i think i don't need to explain why we to check that */
+    
+    if (str->index + 1 + strlen(toAdd) > str->size){
+        while (str->index + 1 + strlen(toAdd) > str->size) /* translate to: as long as we don't have enough memory*/
+        {
+            str->size *= 2;
+        }
+
+        str->data = realloc(str->data, str->size * 2); /* double the buffer size*/
+    }
+
+    int i;
+    for (i = 0; i < strlen(toAdd); i++){
+        str->data[str->index + i] = toAdd[i];
+    }
+
+    str->index += strlen(toAdd);
+    str->data[str->index] = '\0';
+}
+
+void string_add_string(String * str, String toAdd){
+    return string_add_char_pointer(str, toAdd.data);
+}
+
+char string_get_char(String str, unsigned int index){
+    if (index >= str.index) return '\0';
+
+    return str.data[index];
+}
+
+bool string_equals(String str1, String str2){
+    if (str1.index != str2.index) return false;
+
+    int i;
+    for (i = 0; i < str1.index; i++){
+        if (string_get_char(str1, i) != string_get_char(str2, i)) return false;
+    }
+
+    return true;
+}
+
+static unsigned power_2_floor(unsigned x) {
+    int power = 1;
+    while (x >>= 1) power <<= 1;
+    return power;
+}
+
+static unsigned power_2_ceil(unsigned x) {
+    if (x <= 1) return 1;
+    int power = 2;
+    x--;
+    while (x >>= 1) power <<= 1;
+    return power;
+}
