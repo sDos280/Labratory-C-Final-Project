65a5173caa060d9f7b00c082366582887dbb6092
dor283 <sdor2803@gmail.com>
2024-04-12 14:53:52 +0300

    add peek_line_test

---
+++ test/lexer/peek_next_line/peek_next_line.c
@@ -0,0 +1,12 @@
+#include "stdint.h"
+#include "../../../include/lexer.h"
+#include "../../../include/string_util.h"
+
+void main(){
+    lexer_init("\n\n\n\n");
+    peek_next_line();
+    peek_next_line();
+    peek_next_line();
+    peek_next_line();
+    print_token_list();
+}
