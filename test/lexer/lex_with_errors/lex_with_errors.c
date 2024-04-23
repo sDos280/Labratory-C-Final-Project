c46936e829964ca70e99e8fb157e5ee387596e3c
dor283 <sdor2803@gmail.com>
2024-04-23 16:14:02 +0300

    add token list print back to the lex with errors test

--- test/lexer/lex_with_errors/lex_with_errors.c
+++ test/lexer/lex_with_errors/lex_with_errors.c
@@ -3,9 +3,9 @@
 #include "../../../include/string_util.h"
 
 int main(){
-    lexer_init("x: .data 23\nmain: dec x\ninc *r1\n move [*r1, *r2]\n.dataError \"fdsfasdf\"\n");
+    lexer_init("x: .data 23\nmain: dec x\ninc *r1\n mov [*r1, *r2]\n.dataError \"fdsfasdf\"\n");
     lex();
-    /*print_token_list()*/;
+    print_token_list();
     flush_lexer_error_list();
     
     lexer_free();
