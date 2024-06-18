fcc0b89f89cd855455fab1dd5d5e2d4513d77230
dor283 <sdor2803@gmail.com>
2024-06-18 16:19:04 +0300

    add Preprocessor struct

--- include/preprocessor.h
+++ include/preprocessor.h
@@ -7,7 +7,7 @@
 
 typedef struct Macro {
     Token identifier; /* the identifier of the macro */
-    String expansion; /* the expansion of the macro */
+    String expansion; /* the expansion data of the macro */
 
     int start; /* the starting index of the macro (the char starter of MACR) */
     int end; /* the ending index of the macro (the char ender of ENDMACR) */
@@ -18,19 +18,40 @@ typedef struct MacroList {
     struct MacroList * next; /* the next macro */
 } MacroList;
 
+typedef struct PreprocessorTokenError {
+    Token token;  /* the token the error talks about */
+    String message;  /* the message from the caller */
+} PreprocessorTokenError;
+
+typedef struct PreprocessorTokenErrorList {
+    PreprocessorTokenError error; /* the error*/
+    struct PreprocessorTokenErrorList *next;  /* the next error */
+} PreprocessorTokenErrorList;
+
 typedef struct Preprocessor {
     String string; /* the string of the source file after the preprocessor pass */
     MacroList * macroList; /* the macroList of the preprocessor */
+
+    PreprocessorTokenErrorList * errorList; /* the error list of the preprocessor */
+    TokenList * tokens;   /* the token list refrence from the token */
 } Preprocessor;
 
 
 /**
- * Add macro to the end of a macro list
+ * Add a macro to the end of the preprocessor macro list
  *
  * @param preprocessor the preprocessor.
  * @param macro the macro to be added.
 */
-void add_macro_to_macro_list(Preprocessor * preprocessor, Macro macro);
+void add_macro_to_preprocessor_macro_list(Preprocessor * preprocessor, Macro macro);
+
+/**
+ * Add an error to the end of the preprocessor error list
+ *
+ * @param preprocessor the preprocessor.
+ * @param error the error to be added.
+*/
+void add_error_to_preprocessor_macro_list(Preprocessor * preprocessor, PreprocessorTokenError error);
 
 /**
  * Generate a macro list from lexer tokens and the new macroList and append the out source to the preprocessor.
@@ -40,7 +61,6 @@ void add_macro_to_macro_list(Preprocessor * preprocessor, Macro macro);
  * @return a pointer to a memory allocated MacroList.
 */
 void generate_macro_list(Preprocessor * preprocessor, Lexer lexer);
-
 
 
 #endif /* LABRATORY_C_FINAL_PREPROCESSOR */
