3e4b261bfe7ba4d84e0dbf4145e72b8b1437ac60
dor283 <sdor2803@gmail.com>
2024-06-20 17:41:40 +0300

    update error_handlerh

--- include/error_handler.h
+++ include/error_handler.h
@@ -34,4 +34,39 @@ typedef struct ErrorList {
     struct LexerErrorList *next;  /* the next error */
 } ErrorList;
 
+
+typedef struct ErrorHandler {
+    String source;  /* the source file string the errors are in */
+    LexerErrorList * errorList;  /* the error list */
+} ErrorHandler;
+
+/**
+ * Initializes the error handler with the provided source string.
+ * @param handler The error hadler.
+ * @param source The source file string.
+ */
+void error_handler_init(ErrorHandler * handler, String source);
+
+/**
+ * Pushes a new token error to the end of the error handler's error list.
+ * @param error The token error to be added.
+ */
+void error_handler_push_token_error(ErrorHandler * handler, TokenError error);
+
+/**
+ * Pushes a new char error to the end of the error handler's error list.
+ * @param error The char error to be added.
+ */
+void error_handler_push_char_error(ErrorHandler * handler, CharError error);
+
+/**
+ * Flushes (outputs) the error handler's error list to the user.
+ */
+void error_handler_flush_error_list(ErrorHandler * handler);
+
+/**
+ * Frees the memory of the error handler's error list.
+ */
+void error_handler_free_error_list(ErrorHandler * handler);
+
 #endif /* LABRATORY_C_FINAL_PROJECT_ERROR_HANDLER */
