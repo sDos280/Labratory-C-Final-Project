4bd5b27130e39b9d62cf79d4a3bddfe78a14bc13
dor283 <sdor2803@gmail.com>
2024-07-04 15:02:40 +0300

    update node.h

--- include/node.h
+++ include/node.h
@@ -16,9 +16,19 @@ typedef struct EntryNode {
     Token * token;  /* the token (labal) that is reference as entry */
 }EntryNode;
 
+typedef struct EntryNodeList {
+    EntryNode node;  /* the current node */
+    struct EntryNodeList * next; /* the next node */
+}EntryNodeList;
+
 typedef struct ExternalNode {
     Token * token;  /* the token (labal) that is reference as external */
 }ExternalNode;
+
+typedef struct ExternalNodeList {
+    ExternalNode node;  /* the current node */
+    struct ExternalNodeList * next; /* the next node */
+}ExternalNodeList;
 
 typedef struct DataNode {
     TokenRefrenceList * numbers; /* the number in the data node */
@@ -36,23 +46,38 @@ typedef struct InstructionNode {
     bool is_destination_derefrenced; /* true if the destination is derefrenced else false */
 }InstructionNode;
 
-typedef struct NodeList {
+typedef struct InstructionNodeList {
+    InstructionNode node; /* the current instruction node */
+    struct InstructionNodeList * next; /* the next node */
+}InstructionNodeList;
+
+typedef struct GuidanceNodeList {
     union {
         DataNode dataNode;
         StringNode stringNode;
-        InstructionNode instructionNode;
     }node; /* the current node */
 
     NodeKind * kind; /* the node type */
-    struct NodeList * next; /* the next node */
-    
-}NodeList;
+    struct GuidanceNodeList * next; /* the next node */
+}GuidanceNodeList;
 
-typedef struct LabalNode
-{
-    Token * labal; /* the labal identifier */
-    NodeList * nodeList; /* the list of instruction\guidance sentences */
+typedef struct LabalNode {
+    Token * labal; /* the labal identifier, for guidence node list that doesn't have a labal, that would be NULL */
+    InstructionNodeList * instructionNodeList; /* the list of instruction sentences, null if guidance sentences labal */
+    GuidanceNodeList * guidanceNodeList; /* the list of guidance sentences, null if instruction sentences labal  */
 }LabalNode;
+
+typedef struct LabalNodeList {
+    LabalNode labal; /* the current labal */
+    struct LabalNodeList * next; /* the next labal */
+}LabalNodeList;
+
+typedef struct TranslationUnit {
+    ExternalNodeList * externalNodeList; /* the list of all the external nodes */
+    EntryNodeList * entryNodeList; /* the list of all the entry nodes */
+    LabalNodeList * instructionLabalList; /* the instructions labal list */
+    LabalNodeList * guidanceLabalList; /* the guidance labal list */
+}TranslationUnit;
 
 
 #endif /*LABRATORY_C_FINAL_PROJECT_NODE*/
